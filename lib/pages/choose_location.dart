aae0fc4fa95ca46ea7317b4be40aca7c47183628
ruchibajra <ruchibajracharya8@gmail.com>
2024-09-24 23:33:03 +0545

    Asynchronous Code, async and await

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,12 +8,24 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  int counter = 0;
+  void getData() async {
+    //simulate network request for a username
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return 'yoshi';
+    });
+
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return 'web developer';
+    });
+
+    print(
+        '$username - $bio'); //username fn must load and finish in order to move to bio fn.
+  }
 
   @override
   void initState() {
     super.initState();
-    print('initState Function Ran');
+    getData();
   }
 
   @override
@@ -27,14 +39,6 @@ class _ChooseLocationState extends State<ChooseLocation> {
         title: Text('Choose a Location'),
         centerTitle: true,
         elevation: 0, //removes box shadow
-      ),
-      body: ElevatedButton(
-        onPressed: () {
-          setState(() {
-            counter += 1;
-          });
-        },
-        child: Text('counter is $counter'),
       ),
     );
   }
