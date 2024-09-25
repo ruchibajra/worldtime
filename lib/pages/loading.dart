dbe91c9da20eba42ebd1dfd1161bad3601a41dba
ruchibajra <ruchibajracharya8@gmail.com>
2024-09-25 18:37:15 +0545

    Flutter package http

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
-
   @override
   State<Loading> createState() => _LoadingState();
 }
 
 class _LoadingState extends State<Loading> {
+  void getData() async {
+    // Correctly parse the URL using Uri.parse
+    http.Response response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+    Map<String, dynamic> data = jsonDecode(response.body);
+    print(data);
+    print(data['title']);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Loading"),
+      body: Center(
+        child: Text("Loading"),
+      ),
     );
   }
 }
